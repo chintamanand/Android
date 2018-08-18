7b3d15631e609a2153b9c9b5384f6988f509ea94
chintam anand <chintamanand56@gmail.com>
2018-08-18 11:59:05 +0530

    Update Display.java

--- Lab-Work/lab-4/Display.java
+++ Lab-Work/lab-4/Display.java
@@ -27,6 +27,7 @@ public class Display extends Activity {
 		tv.setText(details);
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
